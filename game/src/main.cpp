12a182d4f8354d400de6d71541ce6b5315d8257e
Black-Sheeppp <susana.camarillo.53@gmail.com>
2025-09-21 19:34:30 -0400

    Update main.cpp
    Just a few tweaks before submitting

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -14,6 +14,10 @@ float x = 500;
 float y = 500;
 float frequency = 1.0f;
 float amplitude = 70;
+float speed = 200;
+float angle = 30;
+float startPosX = 500;
+float startPosY = 400;
 
 //Change world state
 void update() {
@@ -28,14 +32,25 @@ void update() {
 //Display world state
 void draw() {
     BeginDrawing();
-    ClearBackground(DARKPURPLE);
-    DrawText("nuh uh", 10, 20, 20, LIGHTGRAY);
+    ClearBackground(BLACK);
+    DrawText("nuh uh", 10, 20, 20, WHITE);
 
 
-    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+   // GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
     DrawText(TextFormat("FPS: %.2i, TIME: %.2f", TARGET_FPS, time), GetScreenWidth() - 200, 30, 15, LIGHTGRAY);
 
-    DrawCircle(x, y, 25, RED);
+    //DrawCircle(x, y, 25, RED);
+
+    GuiSliderBar(Rectangle{ 10, 40, 200, 20 }, "", TextFormat("%.0f", speed), &speed, -100, 1000);
+    GuiSliderBar(Rectangle{ 10, 60, 200, 20 }, "", TextFormat("%.0f", angle), &angle, -180, 180);
+    GuiSliderBar(Rectangle{ 10, 80, 200, 20 }, "", TextFormat("%.0f", startPosX), &startPosX, 100, GetScreenWidth() - 100);
+    GuiSliderBar(Rectangle{ 10, 100, 200, 20 }, "", TextFormat("%.0f", startPosY), &startPosY, 100, GetScreenHeight() - 100);
+
+
+    Vector2 startPos = { startPosX, startPosY };  //GetScreenHeight() - 200
+    Vector2 velocity = { cos(angle * DEG2RAD) * speed, -sin(angle * DEG2RAD ) * speed};
+
+    DrawLineEx(startPos, startPos + velocity, 3, RED);
 
     EndDrawing();
 }
